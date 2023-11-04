e992ec6e2d0b4aeb009c64b316fe8e66a06e8d1f
Rikza Kurnia Almujtaba Lubis <rikza.kurnia@ui.ac.id>
2023-11-04 17:08:10 +0700

    add time comparison

--- TE1/CompareAlgorithm.java
+++ TE1/CompareAlgorithm.java
@@ -29,8 +29,58 @@ public class CompareAlgorithm {
 
         BCIS BCISsort = new BCIS();
         CountingSort countingSort = new CountingSort();
-        BCISsort.sort(smallRandomArray);
-        countingSort.countSort(smallRandomArray, smallRandomArray.length);
+
+        int[] copySmallSorted =Arrays.copyOf(smallSortedArray, smallSortedArray.length);
+        int[] copySmallRandom =Arrays.copyOf(smallRandomArray, smallRandomArray.length);
+        int[] copySmallReversed =Arrays.copyOf(smallReversedArray, smallReversedArray.length);
+
+        int[] copyMediumSorted =Arrays.copyOf(mediumSortedArray, mediumSortedArray.length);
+        int[] copyMediumRandom =Arrays.copyOf(mediumRandomArray, mediumRandomArray.length);
+        int[] copyMediumReversed =Arrays.copyOf(mediumReversedArray, mediumReversedArray.length);
+
+        int[] copyBigSorted =Arrays.copyOf(bigSortedArray, bigSortedArray.length);
+        int[] copyBigRandom =Arrays.copyOf(bigRandomArray, bigRandomArray.length);
+        int[] copyBigReversed =Arrays.copyOf(bigReversedArray, bigReversedArray.length);
+        
+        System.out.println("Compare Small Sorted Array");
+        compareSorting(BCISsort, countingSort, smallSortedArray, copySmallSorted);
+        System.out.println("Compare Small Random Array");
+        compareSorting(BCISsort, countingSort, smallRandomArray, copySmallRandom);
+        System.out.println("Compare Small Reversed Array");
+        compareSorting(BCISsort, countingSort, smallReversedArray, copySmallReversed);
+
+        System.out.println("Compare Medium Sorted Array");
+        compareSorting(BCISsort, countingSort, mediumSortedArray, copyMediumSorted);
+        System.out.println("Compare Medium Random Array");
+        compareSorting(BCISsort, countingSort, mediumRandomArray, copyMediumRandom);
+        System.out.println("Compare Medium Reversed Array");
+        compareSorting(BCISsort, countingSort, mediumReversedArray, copyMediumReversed);
+
+        System.out.println("Compare Big Sorted Array");
+        compareSorting(BCISsort, countingSort, bigSortedArray, copyBigSorted);
+        System.out.println("Compare Big Random Array");
+        compareSorting(BCISsort, countingSort, bigRandomArray, copyBigRandom);
+        System.out.println("Compare Big Reversed Array");
+        compareSorting(BCISsort, countingSort, bigReversedArray, copyBigReversed);
+         
+    }
+
+    public static void compareSorting(BCIS BCISsort, CountingSort countingSort, int[] array, int[] copyArray){
+        long startTimeBCIS = System.nanoTime();
+        BCISsort.sort(array);
+        long endTimeBCIS = System.nanoTime();
+        
+        double executionTimeBCIS = (endTimeBCIS - startTimeBCIS) / 1e6;
+        System.out.println("BCIS Sort Execution Time: " + executionTimeBCIS + " milliseconds");
+
+        long startTimeCountingSort = System.nanoTime();
+        countingSort.countSort(copyArray, copyArray.length);
+        long endTimeCountingSort = System.nanoTime();
+
+        double executionTimeCountingSort = (endTimeCountingSort - startTimeCountingSort) / 1e6; // Convert to milliseconds
+        System.out.println("Counting Sort Execution Time: " + executionTimeCountingSort + " milliseconds");
+
+        System.out.println("");
     }
 
     public static void fillRandomArray(int[] arr) {
@@ -38,14 +88,12 @@ public class CompareAlgorithm {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = rand.nextInt(1000);
         }
-        System.out.println(Arrays.toString(arr));
     }
 
     public static void fillSortedArray(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = i+1; 
         }
-        System.out.println(Arrays.toString(arr));
     }
 
     public static void fillReversedArray(int[] arr) {
@@ -54,7 +102,6 @@ public class CompareAlgorithm {
             arr[i] = last; 
             last-=1;
         }
-        System.out.println(Arrays.toString(arr));
     }
 
 }
--- TE1/CountingSort.java
+++ TE1/CountingSort.java
@@ -41,13 +41,4 @@ public class CountingSort {
     }
   }
 
-  // Driver code
-  public static void main(String args[]) {
-    int[] data = { 4, 2, 2, 8, 3, 3, 1 };
-    int size = data.length;
-    CountingSort cs = new CountingSort();
-    cs.countSort(data, size);
-    // System.out.println("Sorted Array in Ascending Order: ");
-    // System.out.println(Arrays.toString(data));
-  }
 }
